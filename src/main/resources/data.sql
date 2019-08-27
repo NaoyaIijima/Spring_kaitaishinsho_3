5b1ec34f4a70efa4b522104270885e9e6eddb8c7
Naoya Iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-08-27 13:29:56 +0000

    id 2, 3を作成

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 /* 従業員テーブルのデータ（第3章で作成） */
-INSERT INTO employee(employee_id, employee_name, age)
-VALUES(1, '山田太郎', 30);
+INSERT INTO employee(employee_id, employee_name, age) VALUES(1, '山田太郎', 30);
+INSERT INTO employee(employee_id, employee_name, age) VALUES(2, '山田郎', 60);
+INSERT INTO employee(employee_id, employee_name, age) VALUES(3, '山田郎', 60);
